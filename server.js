a12897542891b8593ff22ca5abe228aa1e4583f6
AZZU <orcadehub2@gmail.com>
2026-01-21 20:59:38 +0530

    cors

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(cors({
   origin: [
     'http://localhost:3000',
     'http://localhost:5173', 
-    'https://orcode-fe.vercel.app'
+    /https:\/\/.*\.vercel\.app$/
   ],
   credentials: true
 }))
